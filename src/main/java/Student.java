3f7d37db4ed2edc66384d50d4d6021d0cce2edfa
Dominika Wysocka <dwysocka13@gmail.com>
2021-02-16 18:49:03 +0100

    Zadanie domowe 6

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -4,6 +4,13 @@ public class Student {
     int numerIndeksu;
     String mail;
 
+    public Student(final String imie, final String nazwisko, final int numerIndeksu, final String mail) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.numerIndeksu = numerIndeksu;
+        this.mail = mail;
+    }
+
 
     public  void info (){
         System.out.println("Imie: " + imie);
--- src/main/java/ZadanieDomowe4.java
+++ src/main/java/ZadanieDomowe4.java
@@ -2,23 +2,11 @@ public class ZadanieDomowe4 {
 
     public static void main(String[] args) {
 
-        Student studenciak1 = new Student();
-        studenciak1.imie = "Jan";
-        studenciak1.nazwisko = "Bombela";
-        studenciak1.mail = "jabomb423@polsl.pl";
-        studenciak1.numerIndeksu = 1;
+        Student studenciak1 = new Student("Jan","Bombela",1,"jabomb423@polsl.pl");
 
-        Student studenciak2 = new Student();
-        studenciak2.imie = "Jano";
-        studenciak2.nazwisko = "Bombelo";
-        studenciak2.mail = "jabombolo423@polsl.pl";
-        studenciak2.numerIndeksu = 2;
+        Student studenciak2 = new Student("Jano","Bombelo",2,"jabomblo423@polsl.pl");
 
-        Student studenciak3 = new Student();
-        studenciak3.imie = "TrioJano";
-        studenciak3.nazwisko = "TriBombelo";
-        studenciak3.mail = "jabomb333@polsl.pl";
-        studenciak3.numerIndeksu = 3;
+        Student studenciak3 = new Student("TrioJano","TriBombelo",3,"jabomb333@polsl.pl");
 
         Student[] tablicaStudenciakow = new Student[]{studenciak1, studenciak2, studenciak3};
 
