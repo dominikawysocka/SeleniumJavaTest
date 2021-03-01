729963c815923130e3e1d76653e21019ab3f258c
Dominika Wysocka <dwysocka13@gmail.com>
2021-03-01 22:43:23 +0100

    Selenium asercje

--- src/test/java/FirstSeleniumTest.java
+++ src/test/java/FirstSeleniumTest.java
@@ -2,17 +2,20 @@ import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.WebDriver;
 
 import java.sql.SQLOutput;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Set;
 
 public class FirstSeleniumTest {
 
     @Test
-    public void googleOpenTest() {
+    public void googleOpenTest() throws InterruptedException {
 
         String driverPath = "E://ProjektyzProgramowania//tutorialselenium//src//main//executables//drivers//chromedriver.exe";
         System.setProperty("webdriver.chrome.driver", driverPath);
@@ -21,7 +24,8 @@ public class FirstSeleniumTest {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
 
-        driver.get("E://ProjektyzProgramowania//tutorialselenium//src//main//potrzebneMaterialy//Test.html");
+        //driver.get("E://ProjektyzProgramowania//tutorialselenium//src//main//potrzebneMaterialy//Test.html");
+        driver.get("https://www.google.com/webhp?hl=pl&sa=X&ved=0ahUKEwiZpqGMj43vAhWl-ioKHbyJCCIQPAgI");
 //        WebElement firstNameInput = driver.findElement(By.id("fname"));
 //        WebElement name = driver.findElement(By.name("username"));
 //        WebElement linkTeskt = driver.findElement(By.linkText("Visit W3Schools.com!"));
@@ -106,19 +110,80 @@ public class FirstSeleniumTest {
 //        firstElement.sendKeys("Dominiczka");
 
         //Sprawdzenie czy jest widoczny
-        WebElement topSicretElement = driver.findElement(By.className("topSecret"));
-        if(topSicretElement.isDisplayed()) {
-            System.out.println(topSicretElement.getText());
+//        WebElement topSicretElement = driver.findElement(By.className("topSecret"));
+//        if(topSicretElement.isDisplayed()) {
+//            System.out.println(topSicretElement.getText());
+//        }
+//        else
+//            System.out.println(topSicretElement.getAttribute("textContent"));
+
+        //Sprawdzenie czy element istnieje
+//        boolean exist = checkIfElementExist(By.cssSelector("#fname"), driver);
+//        checkIfElementExist(driver,By.cssSelector("#fname"));
+//        System.out.println(exist);
+
+//        WebElement clickMe = driver.findElement(By.cssSelector("#newPage"));
+//        String currentWindowName = driver.getWindowHandle();
+//        clickMe.click();
+//        Thread.sleep(1000);
+//        switchToNewWindow(driver, currentWindowName);
+//        driver.switchTo().frame(0);
+//        WebElement adreeButton = driver.findElement(By.id("introAgreeButton"));
+//        adreeButton.click();
+//       // switchToNewWindow(driver, currentWindowName);
+//        driver.close();
+//        driver.switchTo().window(currentWindowName);
+//        driver.findElement(By.name("username")).sendKeys("ss");
+
+
+        driver.switchTo().frame(0);
+        WebElement adreeButton = driver.findElement(By.id("introAgreeButton"));
+        adreeButton.click();
+        WebElement wpisywanka = driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[2]/div[1]/div[1]/div/div[2]/input"));
+        wpisywanka.sendKeys("Marcinek");
+        WebElement szukaj = driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[2]/div[1]/div[3]/center/input[1]"));
+        szukaj.click();
+        WebElement linkacz = driver.findElement(By.xpath("//a[@href='http://marcinek.poznan.pl/']"));
+        linkacz.click();
+
+        String expectedTitle = "";
+       // System.out.println("tytuł: " + driver.getTitle());
+        Assert.assertEquals(driver.getTitle(),expectedTitle);
+
+        driver.quit();
+    }
+
+    private void switchToNewWindow(final WebDriver driver, final String currentWindowName) {
+
+        Set<String> windows = driver.getWindowHandles();
+
+        for (String window : windows) {
+            if (!window.equals(currentWindowName)) {
+                driver.switchTo().window(window);
+                System.out.println(driver.getCurrentUrl());
+            }
         }
-        else
-            System.out.println(topSicretElement.getAttribute("textContent"));
 
+    }
 
+    public boolean checkIfElementExist(By locator, WebDriver driver) {
 
+        return driver.findElements(locator).size() > 0;
+    }
 
+    public void checkIfElementExist(WebDriver driver, By locator) {
+        try {
+            driver.findElements(locator);
+            System.out.println("jest");
+        } catch (NoSuchElementException exc) {
+            System.out.println("Ni maaa");
 
-        //driver.quit();
+        }
+
     }
 
 
 }
+
+
+
